1ab602601764e37bdec17ad8156de6bebaaff3c8
Matthew Liu <mliu78@jh.edu>
2020-06-27 03:03:48 -0400

    Updated Contact Page, finished implementing YourHelp page

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import HomePage from './HomePage';
-import ContactPage from './covid-19/ContactPage';
+import ContactPage from './ContactPage';
 import Header from './common/Header';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import NotFoundPAge from './NotFoundPage';
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -5,16 +5,16 @@ function HomePage() {
     return <>
         <div className='jumbotron'>
             <h1>JHU Help</h1>
-            <p>(Still under construction)</p>
         </div>
-        <h4>Check out our Covid-19 pandemic resources!</h4>
-        <Link to='/covid-19' className='btn btn-primary'>Covid-19 Resources</Link>
-        <p></p>
-        <h4>Maryland 911 Alternatives</h4>
-        <Link to='/maryland-911-alternatives' className='btn btn-primary'>Maryland 911 Alternatives</Link>
-        <p></p>
-        <h4>Help us help you!</h4>
-        <Link to='/help-us' className='btn btn-primary'>Help us help you!</Link>
+        <h4>
+            <b>
+                <Link to='/covid-19' >Check out our Covid-19 pandemic resources!</Link>
+                <p></p>
+                <Link to='/maryland-911-alternatives' >Maryland 911 Alternatives</Link>
+                <p></p>
+                <Link to='/help-us' >Help us help you!</Link>
+            </b>
+        </h4>
     </>
 }
 export default HomePage;
