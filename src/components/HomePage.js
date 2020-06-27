727e0579753a934d689471859b568574c895f0a0
Matthew Liu <mliu78@jh.edu>
2020-06-27 03:59:35 -0400

    Some visual changes, works better on mobile now.

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -6,14 +6,14 @@ function HomePage() {
         <div className='bg-image'></div>
         <div className='jumbotron'>
             <div className='bg-text'>
-                <h1 style={{ marginBottom: '3rem' }}>JHU Help</h1>
+                <h1 style={{ marginBottom: '2rem' }}>JHU Help</h1>
                 <h4><b>Check these out!</b></h4>
                 <p></p>
                 <h4>
                     <b>
-                        <Link className='frontPageLink' style={{ marginLeft: '0rem' }} to='/covid-19' >Check out our Covid-19 pandemic resources!</Link>
-                        <Link className='frontPageLink' to='/maryland-911-alternatives' >Maryland 911 Alternatives</Link>
-                        <Link className='frontPageLink' to='/help-us' >Help us help you!</Link>
+                        <div className='frontPageLink'><Link to='/covid-19' >Check out our Covid-19 pandemic resources!</Link></div>
+                        <div className='frontPageLink'><Link to='/maryland-911-alternatives' >Maryland 911 Alternatives</Link></div>
+                        <div className='frontPageLink'><Link to='/help-us' >Help us help you!</Link></div>
                     </b>
                 </h4>
             </div>
