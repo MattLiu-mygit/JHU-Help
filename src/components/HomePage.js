3e4836dbfd2a347b0fa34c8c62c270bb95955089
Matthew Liu <mliu78@jh.edu>
2020-06-27 03:44:07 -0400

    Made some visual imporvements

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,18 +3,22 @@ import { Link } from 'react-router-dom';
 
 function HomePage() {
     return <>
+        <div className='bg-image'></div>
         <div className='jumbotron'>
-            <h1>JHU Help</h1>
+            <div className='bg-text'>
+                <h1 style={{ marginBottom: '3rem' }}>JHU Help</h1>
+                <h4><b>Check these out!</b></h4>
+                <p></p>
+                <h4>
+                    <b>
+                        <Link className='frontPageLink' style={{ marginLeft: '0rem' }} to='/covid-19' >Check out our Covid-19 pandemic resources!</Link>
+                        <Link className='frontPageLink' to='/maryland-911-alternatives' >Maryland 911 Alternatives</Link>
+                        <Link className='frontPageLink' to='/help-us' >Help us help you!</Link>
+                    </b>
+                </h4>
+            </div>
         </div>
-        <h4>
-            <b>
-                <Link to='/covid-19' >Check out our Covid-19 pandemic resources!</Link>
-                <p></p>
-                <Link to='/maryland-911-alternatives' >Maryland 911 Alternatives</Link>
-                <p></p>
-                <Link to='/help-us' >Help us help you!</Link>
-            </b>
-        </h4>
+
     </>
 }
 export default HomePage;
