ca3e9290b96c9d00d2742a415633869546ae9ee2
Matthew Liu <mliu78@jh.edu>
2020-06-30 15:12:50 -0400

    Update createMockDb.js

--- tools/createMockDb.js
+++ tools/createMockDb.js
@@ -3,8 +3,8 @@ const fs = require("fs");
 const path = require("path");
 const mockData = require("./mockData");
 
-const { courses, authors, SGA_Survey_Results } = mockData;
-const data = JSON.stringify({ courses, authors, SGA_Survey_Results });
+const { SGA_Survey_Results } = mockData;
+const data = JSON.stringify({SGA_Survey_Results });
 const filepath = path.join(__dirname, "db.json");
 
 fs.writeFile(filepath, data, function (err) {
