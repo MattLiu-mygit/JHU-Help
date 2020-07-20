9fea09b387edebaeb4ba5d9b6ab579737f871a7a
MattLiu-mygit <mliu78@jh.edu>
2020-07-20 16:35:52 -0400

    Updates?

--- src/api/SgaSurveyResultsApi.js
+++ src/api/SgaSurveyResultsApi.js
@@ -1,28 +1,44 @@
-import { handleResponse, handleError } from "./apiUtils";
-const baseUrl = process.env.REACT_APP_API_URL + "/SGA_Survey_Results/";
+import { handleResponse, handleError } from './apiUtils';
+
+const MongoClient = require('mongodb').MongoClient;
+const mongoUrl =
+  'mongodb+srv://matt-liu:Kevin0122@jhu-help.eax6k.mongodb.net/JHU-Help?retryWrites=true&w=majority';
+const uri = mongoUrl;
 
 export function getData() {
-    return fetch(baseUrl)
-        .then(handleResponse)
-        .catch(handleError);
+  const client = new MongoClient(uri, { useNewUrlParser: true });
+  client.connect((err) => {
+    const collection = client.db('test').collection('devices');
+    // perform actions on the collection object
+    client.close();
+  });
 }
 
-export function saveData(data) {
-    return fetch(baseUrl + (data.id || ""), {
-        method: data.id ? "PUT" : "POST", // POST for create, PUT to update when id already exists.
-        headers: { "content-type": "application/json" },
-        body: JSON.stringify({
-            ...data,
-            // Parse authorId to a number (in case it was sent as a string).
-            authorId: parseInt(data.authorId, 10)
-        })
-    })
-        .then(handleResponse)
-        .catch(handleError);
-}
+// For JSON-based api
+// const baseUrl = process.env.REACT_APP_API_URL + "/SGASurveyResults/";
 
-export function deleteData(dataId) {
-    return fetch(baseUrl + dataId, { method: "DELETE" })
-        .then(handleResponse)
-        .catch(handleError);
-}
+// export function getData() {
+//     return fetch(baseUrl)
+//         .then(handleResponse)
+//         .catch(handleError);
+// }
+
+// export function saveData(data) {
+//     return fetch(baseUrl + (data.id || ""), {
+//         method: data.id ? "PUT" : "POST", // POST for create, PUT to update when id already exists.
+//         headers: { "content-type": "application/json" },
+//         body: JSON.stringify({
+//             ...data,
+//             // Parse authorId to a number (in case it was sent as a string).
+//             authorId: parseInt(data.authorId, 10)
+//         })
+//     })
+//         .then(handleResponse)
+//         .catch(handleError);
+// }
+
+// export function deleteData(dataId) {
+//     return fetch(baseUrl + dataId, { method: "DELETE" })
+//         .then(handleResponse)
+//         .catch(handleError);
+// }
--- src/components/covid-19/SurveyTable.js
+++ src/components/covid-19/SurveyTable.js
@@ -1,35 +1,100 @@
-import React from 'react';
-//import { getData } from '../../api/SgaSurveyResultsApi';
+import React, { useEffect, useState } from 'react';
+import { getData } from '../../api/SgaSurveyResultsApi';
 import { Link } from 'react-router-dom';
 
 const SurveyTable = () => {
-    //const [SGAData, setSGAData] = useState([]);
-    /*useEffect(() => {
-        getData().then(data => setSGAData(data));
-    }, []);*/
-    return <>
-        <table className='table'>
-            <thead>
-                <tr>
-                    <th>Issue</th>
-                    <th>Response Percent</th>
-                </tr>
-            </thead>
-            <tbody>
-                {//SGAData.map(data => <tr><td>{data.label}</td><td>{data.percentage}</td></tr>)
-                }
-                <tr><td><Link to='/covid-19/mental-emotional'>Personal Mental Health Issues</Link></td><td>46.77%</td></tr>
-                <tr><td><Link to='/covid-19/mental-emotional'>Take care of relatives/siblings/etc.</Link></td><td>33.48%</td></tr>
-                <tr><td><Link to='/covid-19/mental-emotional'>Unstable home environment</Link></td><td>29.01%</td></tr>
-                <tr><td><Link to='/covid-19/unstable-income'>Unstable income</Link></td><td>25.31%</td></tr>
-                <tr><td><Link to='/covid-19/resource-insecurity'>Lack of reliable internet</Link></td><td>	25.11%</td></tr>
-                <tr><td><Link to='/covid-19/resource-insecurity'>Lack of resources</Link></td><td>23.69%</td></tr>
-                <tr><td><Link to='/covid-19/college-help'>Disadvantageous time zones</Link></td><td>22.13%</td></tr>
-                <tr><td>Other</td><td>15.65%</td></tr>
-                <tr><td><Link to='/covid-19/unstable-income'>Working to supplement income</Link></td><td>9.73%</td></tr>
-                <tr><td><Link to='/covid-19/mental-emotional'>Covid-19 related sickness/death</Link></td><td>5.23%</td></tr>
-            </tbody>
-        </table>
+  const [SGAData, setSGAData] = useState([]);
+  useEffect(() => {
+    getData().then((data) => setSGAData(data));
+  }, []);
+  return (
+    <>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Issue</th>
+            <th>Response Percent</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            //SGAData.map(data => <tr><td>{data.label}</td><td>{data.percentage}</td></tr>)
+          }
+          <tr>
+            <td>
+              <Link to="/covid-19/mental-emotional">
+                Personal Mental Health Issues
+              </Link>
+            </td>
+            <td>46.77%</td>
+          </tr>
+          <tr>
+            <td>
+              <Link to="/covid-19/mental-emotional">
+                Take care of relatives/siblings/etc.
+              </Link>
+            </td>
+            <td>33.48%</td>
+          </tr>
+          <tr>
+            <td>
+              <Link to="/covid-19/mental-emotional">
+                Unstable home environment
+              </Link>
+            </td>
+            <td>29.01%</td>
+          </tr>
+          <tr>
+            <td>
+              <Link to="/covid-19/unstable-income">Unstable income</Link>
+            </td>
+            <td>25.31%</td>
+          </tr>
+          <tr>
+            <td>
+              <Link to="/covid-19/resource-insecurity">
+                Lack of reliable internet
+              </Link>
+            </td>
+            <td> 25.11%</td>
+          </tr>
+          <tr>
+            <td>
+              <Link to="/covid-19/resource-insecurity">Lack of resources</Link>
+            </td>
+            <td>23.69%</td>
+          </tr>
+          <tr>
+            <td>
+              <Link to="/covid-19/college-help">
+                Disadvantageous time zones
+              </Link>
+            </td>
+            <td>22.13%</td>
+          </tr>
+          <tr>
+            <td>Other</td>
+            <td>15.65%</td>
+          </tr>
+          <tr>
+            <td>
+              <Link to="/covid-19/unstable-income">
+                Working to supplement income
+              </Link>
+            </td>
+            <td>9.73%</td>
+          </tr>
+          <tr>
+            <td>
+              <Link to="/covid-19/mental-emotional">
+                Covid-19 related sickness/death
+              </Link>
+            </td>
+            <td>5.23%</td>
+          </tr>
+        </tbody>
+      </table>
     </>
-}
+  );
+};
 export default SurveyTable;
